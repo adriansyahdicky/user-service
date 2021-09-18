67494d32a094a7918a4774255ab63f07b8c923bb
dicky <dicky@dfx-id.com>
2021-09-19 04:58:01 +0700

    oke

--- src/main/java/com/id/simian/userservice/AuthenticationServiceApplication.java
+++ src/main/java/com/id/simian/userservice/AuthenticationServiceApplication.java
@@ -2,7 +2,7 @@ package com.id.simian.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//
 @SpringBootApplication
 public class AuthenticationServiceApplication {
 
